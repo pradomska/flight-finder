d4b2e68f063aa3e0a6cef889e6cdb4d7d4261ff0
Paulina Radomska <paulinaradomska10@wp.pl>
2023-05-01 16:07:26 +0200

    end of project

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,32 @@
+from pprint import pprint
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/080db92c65c44c96aead601ec819cb21/flightDeal/price"
+SHEETY_TOKEN = "080db92c65c44c96aead601ec819cb21"
+
+bearer_headers = {
+        "Authorization": f"Bearer {SHEETY_TOKEN}"
+    }
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=bearer_headers)
+        data = response.json()
+        self.destination_data = data["price"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,21 @@
+from twilio.rest import Client
+
+TWILIO_SID = "ACed99dee86823f4e65269e783a7604e42"
+TWILIO_AUTH_TOKEN = "326ccefd7adab7a75138b24830f66492"
+TWILIO_VIRTUAL_NUMBER = "+16205268968"
+TWILIO_VERIFIED_NUMBER = "+48600171513"
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
